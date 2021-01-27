c7d7bfbedaeb712e5cc5ee1949e2fb5dea71bfa2
Xiao Liang <xiliang@redhat.com>
2021-01-27 21:33:50 +0800

    add RHEL-9 branch as some pkg missing now
    Signed-off-by: Xiao Liang <xiliang@redhat.com>

--- vars/ec2_prepare_test_ami.groovy
+++ vars/ec2_prepare_test_ami.groovy
@@ -36,6 +36,8 @@ def call() {
         pkgs="install,automake,autoconf,sysstat,gcc,unzip,wget,quota,bzip2,iperf3,pciutils,fio,psmisc,expect,ntpdate,perf,nvme-cli,pciutils,fio,git,tar,nfs-utils,libvirt,qemu-kvm,kernel-debug,python3,dracut-fips"
     elif [[ $COMPOSE_ID =~ 'RHEL-8' ]]; then
         pkgs="make,automake,autoconf,sysstat,gcc,unzip,wget,quota,bzip2,iperf3,pciutils,fio,psmisc,expect,perf,nvme-cli,pciutils,fio,php-cli,php-xml,php-json,libaio-devel,blktrace,fio,nvme-cli,git,tar,rng-tools,nfs-utils,libvirt,qemu-kvm,kernel-debug,python3,dracut-fips"
+    elif [[ $COMPOSE_ID =~ 'RHEL-9' ]]; then
+        pkgs="make,automake,autoconf,sysstat,gcc,unzip,wget,quota,bzip2,iperf3,pciutils,fio,psmisc,expect,perf,nvme-cli,pciutils,fio,libaio-devel,blktrace,fio,nvme-cli,git,tar,rng-tools,nfs-utils,libvirt,qemu-kvm,python3,dracut-fips"
     elif [[ $COMPOSE_ID =~ 'CentOS-Stream' ]]; then
         ssh_user='centos'
         pkgs="make,automake,autoconf,sysstat,gcc,unzip,wget,quota,bzip2,iperf3,pciutils,fio,psmisc,expect,perf,nvme-cli,pciutils,fio,php-cli,php-xml,php-json,libaio-devel,blktrace,fio,nvme-cli,git,tar,rng-tools,nfs-utils,libvirt,qemu-kvm,kernel-debug,python3,dracut-fips"
