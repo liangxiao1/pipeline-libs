5b11e5c11a2254193d96f615a8e31a3179f41777
Xiao Liang <xiliang@redhat.com>
2021-01-29 22:18:07 +0800

    specify target when call amis_status_check.py
    Signed-off-by: Xiao Liang <xiliang@redhat.com>

--- vars/ec2_dva_cfg_dump.groovy
+++ vars/ec2_dva_cfg_dump.groovy
@@ -15,7 +15,7 @@ def call() {
         pip install -U PyYAML
         python ${WORKSPACE}/mini_utils/dva_dump_images.py --task_url ${job_env.JOB_TASK_URL} --dir ${WORKSPACE} -d
         python ${WORKSPACE}/mini_utils/dva_config_generate.py --pubkeyfile ${WORKSPACE}/data/virtqe_s1.pub --sshkeyfile ${WORKSPACE}/data/virtqe_s1.pem --target ${job_env.JOB_TARGET} --tokenfile /etc/dva_keys.yaml --output ${job_env.DVA_CFG}
-        python ${WORKSPACE}/mini_utils/amis_status_check.py --task_url ${job_env.JOB_TASK_URL}
+        python ${WORKSPACE}/mini_utils/amis_status_check.py --task_url ${job_env.JOB_TASK_URL} --target ${job_env.JOB_TARGET}
         deactivate
         """
     )
