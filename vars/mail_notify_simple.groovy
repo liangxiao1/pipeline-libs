dde38b99178f6ccca71b49a297758b21a59e13ee
Xiao Liang <xiliang@redhat.com>
2021-02-04 10:52:39 +0800

    correct attachLog property
    Signed-off-by: Xiao Liang <xiliang@redhat.com>

--- vars/mail_notify_simple.groovy
+++ vars/mail_notify_simple.groovy
@@ -6,7 +6,7 @@ def call(String sender, String receiver, boolean attach_log=false) {
 ${env.JOB_NAME} - ${env.BUILD_DISPLAY_NAME} - ${currentBuild.currentResult}
 Check console output at ${env.BUILD_URL}
         """,
-        attachLog: ${attach_log},
+        attachLog: "${attach_log}",
         subject: "${env.JOB_NAME} - ${env.BUILD_DISPLAY_NAME} - ${currentBuild.currentResult}",
         from: "${sender}",
         to: "${receiver}"
